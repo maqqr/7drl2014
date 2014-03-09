b7f803a61c6440e3ec95632dcdabf9d780e4f735
maqqr <maqqr@server.fake>
2014-03-09 15:59:50 +0200

    removed GLUT support

--- Main.hs
+++ Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import Data.Char
-import ConsoleGLUT
+import Console
 
 
-display :: IO ()
-display = do
+mainloop :: Console -> Bool -> IO ()
+mainloop _ False = return ()
+mainloop con True = do
     clearConsole
 
     mapM_ (\c -> drawChar (ord '@') (c, 5)) [1..30]
@@ -13,11 +14,9 @@ display = do
     drawString "Hello World!" (2, 2)
     drawString "Testing" (5, 3)
 
-    flushConsole Console
-
---mainloop con . not =<< consoleShouldClose con
+    flushConsole con
+    mainloop con . not =<< consoleShouldClose con
 
 
 main :: IO ()
-main = runConsole 80 50 "Necromancer Simulator 2014" $ display
-
+main = withConsole 80 60 "Necromancer Simulator 2014" $ \con -> mainloop con True
