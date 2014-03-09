ff68570eb01b56634009fcf4450660134fc02d2c
Simo Rinne <rinne.simo@gmail.com>
2014-03-09 22:02:42 +0200

    added basic framework for the game

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char
+import qualified Data.Set as S
 import Control.Monad
 import Control.Monad.State
 import Graphics.UI.GLFW (Key(..))
@@ -10,18 +11,51 @@ import Console
 
 type GameState a = StateT Game IO a
 
-mainloop :: Bool -> Console -> GameState ()
-mainloop False _  = return ()
-mainloop True con = do
+type ConsoleLoop = Bool -> Console -> GameState ()
+
+consoleLoop :: Console -> ConsoleLoop -> GameState ()
+consoleLoop con f = lift (consoleIsRunning con) >>= \run -> lift (flushConsole con) >>= f run
+
+townmap :: ConsoleLoop
+townmap False _  = return ()
+townmap True con = do
     lift $ do
         clearConsole
+        drawString "Townmap" (0, 0)
 
-        mapM_ (\c -> drawChar (ord '@') (c, 5)) [1..30]
+    consoleLoop con townmap
 
-        drawString "Hello World!" (2, 2)
-        drawString "Testing" (5, 3)
+worldmap :: ConsoleLoop
+worldmap False _  = return ()
+worldmap True con = do
+    lift $ do
+        clearConsole
+        drawString "(draw map here)" (35, 24)
 
-    lift (consoleIsRunning con) >>= \run -> lift (flushConsole con) >>= mainloop run
+    consoleLoop con worldmap
+
+
+
+characterCreation :: ConsoleLoop
+characterCreation = characterCreation' ""
+    where
+        characterCreation' :: String -> ConsoleLoop
+        characterCreation' _    False _  = return ()
+        characterCreation' name True con = do
+            lift $ do
+                clearConsole
+                drawString ("Enter your name: " ++ name) (3, 5)
+
+            case con `keyPressed` Key'Enter of
+                True  -> consoleLoop con worldmap
+                False -> let writtenCharacters = map keyToChar (pressedKeys con)
+                         in consoleLoop con (characterCreation' $ name ++ writtenCharacters)
+
+        pressedKeys :: Console -> [Key]
+        pressedKeys con = filter (\k -> con `keyPressed` k) (S.elems $ input con)
+
+        keyToChar :: Key -> Char
+        keyToChar key = toLower . last . show $ key
 
 
 mainmenu :: Bool -> Console -> IO ()
@@ -29,12 +63,13 @@ mainmenu False _  = return ()
 mainmenu True con = do
     clearConsole
     drawString "Necromancer Simulator 2014" (10, 3)
-    case con `keyPressed` Key'A of
-        True  -> newGame >>= runGame con
-        False -> consoleIsRunning con >>= \run -> flushConsole con >>= mainmenu run
+    drawString "(S)tart New Game" (5, 8)
+    drawString "(Q)uit Game" (5, 10)
+    when (con `keyPressed` Key'S) $ newGame >>= runGame con
+    unless (con `keyPressed` Key'Q) $ consoleIsRunning con >>= \run -> flushConsole con >>= mainmenu run
 
 runGame :: Console -> Game -> IO ()
-runGame = evalStateT . mainloop True
+runGame = evalStateT . townmap True
 
 main :: IO ()
 main = withConsole 80 60 "Necromancer Simulator 2014" (mainmenu True)
